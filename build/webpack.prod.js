a0175c598d22018f93c7942bd45ee01881c4b7f3
Rachot Moragraan <janechii@gmail.com>
2018-02-19 23:48:12 -0800

    Production build

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,10 +1,15 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 const webpack = require('webpack');
 const MinifyPlugin = require("babel-minify-webpack-plugin");
 
 module.exports = merge(common, {
+  output: {
+    filename: 'map-viewer.js',
+    path: path.resolve(__dirname, '../dist')
+  },
   plugins: [
     new webpack.optimize.ModuleConcatenationPlugin(),
     new MinifyPlugin()
