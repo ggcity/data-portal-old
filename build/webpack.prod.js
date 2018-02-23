a7b2f6b9fecbf7e47e37d754fea5b356cc36ba8f
Rachot Moragraan <moo@ci.garden-grove.ca.us>
2018-02-23 08:35:16 -0800

    Upgraded all dependencies to 0.5.0, fixed Chrome

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -5,6 +5,10 @@ const webpack = require('webpack');
 const MinifyPlugin = require("babel-minify-webpack-plugin");
 
 module.exports = merge(common, {
+  output: {
+    filename: 'gg-map-viewer.js',
+    path: path.resolve(__dirname, '../dist')
+  },
   plugins: [
     new webpack.optimize.ModuleConcatenationPlugin(),
     new MinifyPlugin()
